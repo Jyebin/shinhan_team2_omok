3fbb266de83c8b2e18947e468d180d98c5d63b41
soyoungxxx <otherplet@gmail.com>
2024-05-08 21:30:26 +0900

    feat: Add MainPageDAO() - MainPageDAO 생성자 작성

--- omok/src/main/java/DAO/MainPageDAO.java
+++ omok/src/main/java/DAO/MainPageDAO.java
@@ -1,2 +1,24 @@
-package DAO;public class MainPageDAO {
+package DAO;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class MainPageDAO {
+    private Connection con;
+    private PreparedStatement pstmt;
+    private DataSource dataFactory;
+
+    public MainPageDAO() {
+        try {
+            Context ctx = new InitialContext();
+            Context envContext = (Context) ctx.lookup("java:/comp/env");
+            dataFactory = (DataSource) envContext.lookup("jdbc/mysql");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
