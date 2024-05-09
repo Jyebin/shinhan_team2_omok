e9fb15777f9c6b06935e68e81732f4aaa39e763e
soyoungxxx <otherplet@gmail.com>
2024-05-09 20:48:50 +0900

    feat: Add DB Connection Code - DB 커넥션 풀 방식으로 연결하는 코드 추가

--- omok/src/main/java/DAO/MainPageDAO.java
+++ omok/src/main/java/DAO/MainPageDAO.java
@@ -2,24 +2,48 @@ package DAO;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import VO.UserVO;
 
 public class MainPageDAO {
-    private Connection con;
-    private PreparedStatement pstmt;
+    private Connection con = null;
     private DataSource dataFactory;
 
     public MainPageDAO() {
         try {
-            String dbURL = "jdbc:mysql://localhost:3306/JSP";
-            String dbID = "testuser";
-            String dbPW = "test1234";
-            Class.forName("com.mysql.jdbc.Driver");
+            Context init = new InitialContext();
+            dataFactory = (DataSource) init.lookup("java:comp/env/jdbc/mysql");
+            System.out.println("Success");
         } catch (Exception e) {
+            System.out.println("Fail");
             e.printStackTrace();
         }
     }
 
+    public List<UserVO> getMemberList() {
+        List<UserVO> list = new ArrayList<>();
+        PreparedStatement pstmt = null;
+        try {
+            con = dataFactory.getConnection();
+            String query = "select user_name from member order by user_win_cnt desc";
+            pstmt = con.prepareStatement(query);
+            ResultSet rs = pstmt.executeQuery();
+            while (!rs.next()) {
+                UserVO user = new UserVO();
+                user.setUserName(rs.getString("user_name"));
+                list.add(user);
+            }
+            rs.close();
+            pstmt.close();
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
