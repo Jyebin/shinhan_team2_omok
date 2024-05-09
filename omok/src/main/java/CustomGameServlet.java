4f23930723397de983edea25cfccceeace03a8fc
Jyebin <yebin0322@naver.com>
2024-05-10 03:21:25 +0900

    Feat : CustomGameServlet jsp 연결 - CustomGamePage.jsp 연결 및 테스트 완료

--- omok/src/main/java/CustomGameServlet.java
+++ omok/src/main/java/CustomGameServlet.java
@@ -3,15 +3,21 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "Servlet", value = "/Servlet")
+@WebServlet(name = "CustomGameServlet", value = "/custom-game")
 public class CustomGameServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doHandle(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doHandle(request, response);
+    }
+    protected void doHandle(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        // 메인 페이지 로직
+        // 랭킹을 위해 멤버 정보 받아오기
 
+        req.getRequestDispatcher("/WEB-INF/view/CustomGamePage.jsp").forward(req, res);
     }
 }
