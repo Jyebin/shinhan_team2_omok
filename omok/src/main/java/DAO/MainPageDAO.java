62a849a9cec3a37c226ce3a40a087f817a83e646
soyoungxxx <otherplet@gmail.com>
2024-05-10 17:41:47 +0900

    refactor: Change data type - 넘겨받는 list의 타입을 MemberVO에서 String으로 변환

--- omok/src/main/java/DAO/MainPageDAO.java
+++ omok/src/main/java/DAO/MainPageDAO.java
@@ -25,37 +25,23 @@ public class MainPageDAO {
         }
     }
 
-    public List<UserVO> getMemberList() {
-        List<UserVO> list = new ArrayList<>();
+    public List<String> getMemberList() {
+        List<String> list = new ArrayList<>();
         PreparedStatement pstmt = null;
         try {
             con = dataSource.getConnection();
-            String query = "select * from member order by user_id";
+            String query = "select * from member order by user_win_cnt";
             pstmt = con.prepareStatement(query);
             ResultSet rs = pstmt.executeQuery(query);
             while (rs.next()) {
-                // 한 행(회원 1명당) 처리
-                int id = rs.getInt("user_id");
-                String pw = rs.getString("user_pw");
                 String name = rs.getString("user_name");
-                int userWinCnt = rs.getInt("user_win_cnt");
-                int userGameCnt = rs.getInt("user_win_cnt");
-
-                UserVO vo = new UserVO();
-                vo.setUserId(id);
-                vo.setUserPw(pw);
-                vo.setUserName(name);
-                vo.setUserWinCnt(userWinCnt);
-                vo.setUserGameCnt(userGameCnt);
-                list.add(vo);
+                list.add(name);
             }
             rs.close();
             pstmt.close();
             con.close();
-            System.out.println("read success");
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("read fail");
         }
         return list;
     }
--- omok/src/main/java/MainPageServlet.java
+++ omok/src/main/java/MainPageServlet.java
@@ -24,17 +24,19 @@ public class MainPageServlet extends HttpServlet {
     protected void doHandle(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         res.setContentType("text/html; charset=utf-8");
         // 메인 페이지 로직
+
         // 랭킹을 위해 멤버 정보 받아오기
         MainPageDAO dao = new MainPageDAO();
-        List<UserVO> list = dao.getMemberList();
+        List<String> list = dao.getMemberList();
         req.setAttribute("memberList", list);
         for (int i = 0; i < list.size(); i++){
-            System.out.println(i + " " + list.get(i).getUserName());
+            System.out.println(i + " " + list.get(i));
         }
         // 1~3등은 따로 정보 넘겨주기
-        req.setAttribute("firstMember", list.get(0).getUserName());
-        req.setAttribute("secondMember", list.get(1).getUserName());
-        req.setAttribute("thirdMember", list.get(2).getUserName());
+        req.setAttribute("firstMember", list.get(0));
+        req.setAttribute("secondMember", list.get(1));
+        req.setAttribute("thirdMember", list.get(2));
+        // 유저 검색
         req.getRequestDispatcher("/WEB-INF/view/MainPage.jsp").forward(req, res);
     }
 }
