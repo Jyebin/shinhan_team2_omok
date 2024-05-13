3b161ab021da30eed932b36f0a60ffc04a087b88
soyoungxxx <otherplet@gmail.com>
2024-05-14 02:12:22 +0900

    feat: Add CreateRoomServlet - CreateRoomServlet 변경 및 random, custom room 생성 기능 추가

--- omok/src/main/java/CustomGameServlet.java
+++ omok/src/main/java/CustomGameServlet.java
@@ -1,5 +1,4 @@
-import DAO.CustomGameDAO;
-
+import DAO.GameDAO;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -12,46 +11,22 @@ import java.security.SecureRandom;
 public class CustomGameServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        StringBuilder roomCode = createRandomText(); //roomCode 생성
-        System.out.println("입장 코드 : " + roomCode);
-
-        CustomGameDAO customGameDAO = new CustomGameDAO();
-        customGameDAO.createGame(roomCode.toString()); //저장
-        StringBuilder gameId = customGameDAO.findRoomId(roomCode.toString());
-        request.setAttribute("roomCode", roomCode); //프론트로 전송
-        request.setAttribute("game_id", gameId);
-
-        System.out.println("게임 방 id : " + gameId);
         doHandle(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String roomCode = request.getParameter("roomCode"); //roomCode값을 받아옴
-        if (roomCode != null) {
-            System.out.println("roomCode:" + roomCode);
-            CustomGameDAO customGameDAO = new CustomGameDAO();
-            customGameDAO.changeIsCustom(roomCode);
+        String roomId = request.getParameter("roomId"); //roomCode값을 받아옴
+        if (roomId != null) {
+            System.out.println("roomCode:" + roomId);
+            GameDAO gameDAO = new GameDAO();
+            gameDAO.changeIsCustom(Integer.parseInt(roomId));
         } else {
             System.out.println("roomCode가 null값입니다");
         }
     }
 
     protected void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // 메인 페이지 로직
-        // 랭킹을 위해 멤버 정보 받아오기
         request.getRequestDispatcher("/WEB-INF/view/CustomGamePage.jsp").forward(request, response);
     }
-
-    public static StringBuilder createRandomText() { //방 입장 코드 생성 로직
-        String range = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //4자리 수를 추출할 문자열
-        SecureRandom secureRandom = new SecureRandom();
-        StringBuilder roomCode = new StringBuilder();
-        for (int i = 0; i < 4; i++) {
-            int rand = secureRandom.nextInt(range.length()); //문자열 범위 안에서 하나 선택
-            roomCode.append(range.charAt(rand));
-        }
-        return roomCode;
-    }
-
 }
--- omok/src/main/java/RandomGameServlet.java
+++ omok/src/main/java/RandomGameServlet.java
@@ -1,6 +1,3 @@
-import DAO.RandomGameDAO;
-import VO.GameListVO;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,14 +21,6 @@ public class RandomGameServlet extends HttpServlet {
     protected void doHandle(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         res.setContentType("text/html; charset=utf-8");
         req.getRequestDispatcher("/WEB-INF/view/RandomGamePage.jsp").forward(req, res);
-
-        // 방 생성
-//        new RandomGameDAO().createGameRoom();
-
-        // 방 전환
-//        GameListVO vo = new GameListVO();
-//        vo.setGameId(1);
-//        new RandomGameDAO().switchCustomRoom(vo, CustomGameServlet.createRandomText().toString());
     }
 
 }
