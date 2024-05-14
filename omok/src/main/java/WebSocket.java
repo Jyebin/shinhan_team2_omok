ff00f5f73e33c064a6184f5f3411fd372a0432f0
wing-beat <wingbeat1115@gmail.com>
2024-05-14 18:00:03 +0900

    Feat: Add Live Chatting Function - WebSocket 베이스 실시간 채팅 기능 구현 - RandomGamePage만 수정 완료 (CustomGamePage.jsp 수정 필요)

--- omok/src/main/java/WebSocket.java
+++ omok/src/main/java/WebSocket.java
@@ -1,3 +1,7 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.io.IOException;
 import java.util.*;
 
@@ -12,12 +16,10 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint("/{room}/{type}")
 public class WebSocket {
     // 대기 방, 꽉찬 방 리스트
-    private static Map<String, List<Session>> waitingRoom = new LinkedHashMap<>();
-    private static Map<String, List<Session>> fullRoom = new HashMap<>();
-    // 이 메모리가 공유가 안 됨,, 각자 객체를 생성해서 그런ㄷ ㅡㅅ
+     private static Map<String, List<Session>> waitingRoom = new LinkedHashMap<>();
+     private static Map<String, List<Session>> fullRoom = new LinkedHashMap<>();
 
     // WebSocket으로 브라우저가 접속하면 요청되는 함수
-    // type -> create, enter로 나뉨 ,, 로직 다시 짜야됨
     @OnOpen
     public void handleOpen(Session sess, @PathParam("room") String room, @PathParam("type") String type) throws IOException {
         System.out.println("socket loading");
@@ -27,16 +29,19 @@ public class WebSocket {
             List<Session> newRoom = new ArrayList<>();
             newRoom.add(sess);
             waitingRoom.put(room, newRoom);
-            System.out.println(waitingRoom.size() + " 개 존재");
-        }
-        else if ("enter".equals(type)) {
+        } else if ("enter".equals(type)) {
             // 방 입장 로직
             List<Session> findRoom = waitingRoom.get(room);
             if (findRoom == null) { // 방이 없을 경우
+                System.out.println("create- "+waitingRoom.size() + " 개 존재");
                 sess.getAsyncRemote().sendText("방이 존재하지 않습니다.");
                 sess.close();
             }
+            System.out.println("enter room: "+room);
+            System.out.println("waitingRoom: "+waitingRoom);
+            System.out.println("waitingRoomSize: "+waitingRoom.size());
             waitingRoom.get(room).add(sess); // 기존 방 session list에 새로 입장한 사람 session 넣고
+
             fullRoom.put(room, waitingRoom.get(room)); // room, arraylist 그대로 fullRoom에 삽입
             waitingRoom.remove(room); // 기존 대기방에서 삭제
         }
@@ -45,11 +50,33 @@ public class WebSocket {
 
     // WebSocket으로 메시지가 오면 요청되는 메서드
     @OnMessage
-    public void handleMessage(Session recieveSession, String message) {
-        // 메시지 내용을 콘솔에 출력
+    public void handleMessage(Session recieveSession, String message, @PathParam("type") String type, @PathParam("room") String room) throws IOException, ParseException {
+        System.out.println(room);
         System.out.println(message);
-
-
+        System.out.println(type);
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = (JSONObject)parser.parse(message);
+        System.out.println("jsonObject.get(\"event\") : "+jsonObject.get("event"));
+        if(jsonObject.get("event").equals("chat")){
+            if (this.fullRoom.get(room)==null){
+                JSONObject data = new JSONObject();
+                data.put("room", room);
+                data.put("type", type);
+                data.put("message", jsonObject.get("message"));
+                data.put("event" , "chat");
+                recieveSession.getBasicRemote().sendText(data.toString());
+            } else {
+                for(int i = 0; i < this.fullRoom.get(room).size(); ++i) {
+                    Session waitingSession = this.fullRoom.get(room).get(i);
+                    JSONObject data = new JSONObject();
+                    data.put("room", room);
+                    data.put("type", type);
+                    data.put("message", jsonObject.get("message"));
+                    data.put("event" , "chat");
+                    waitingSession.getBasicRemote().sendText(data.toString());
+                }
+            }
+        }
     }
 
     // WebSocket과 브라우저가 접속이 끊기면 요청되는 함수
